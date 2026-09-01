b8ffcbd2f8b4beba50ec30bca5c8d30ec17080fb
haruka2243 <asuka2246shizu@gmail.com>
2026-09-01 13:27:37 +0900

    更新

--- characters.js
+++ characters.js
@@ -1,1 +1,22 @@
+const characters = [
+    ["髙橋先生", "髙橋先生"],
+    ["増田先生", "増田先生"]
+];
 
+const gallery = document.getElementById("gallery");
+
+characters.forEach(character => {
+
+    const name = character[0];
+    const image = character[1];
+
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+        <img src="${image}" alt="${name}">
+        <div class="name">${name}</div>
+    `;
+
+    gallery.appendChild(card);
+});
