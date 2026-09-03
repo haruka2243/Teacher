cf1aa85b726bfca6594a4c560b73888bca3b9016
haruka2243 <asuka2246shizu@gmail.com>
2026-09-03 13:34:58 +0900

    更新

--- characters.js
+++ characters.js
@@ -1,22 +1,22 @@
 const teachers = [
-    ["髙橋先生", "髙橋先生.jpg", "3年学年主任　少林寺部顧問"],
-    ["増田先生", "増田先生.jpg", "情報の先生"],
-    ["藤野先生", "藤野先生.jpg", "3年1組の担任　バドミントン部副顧問"],
-    ["西田先生", "西田先生.jpg", "3年2組の担任"],
-    ["會見先生", "會見先生.jpg", "2年学年主任　2年1組の担任　バドミントン部顧問"],
-    ["前田先生", "前田先生.jpg", ""],
-    ["若原先生", "若原先生.jpg", "国語の先生"],
-    ["玉本先生", "玉本先生.jpg", "美術の先生"],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""],
-    ["先生", "先生.jpg", ""]
+    ["先生", "3年学年主任　少林寺部顧問"],
+    ["先生", "情報の先生"],
+    ["先生", "3年1組の担任　バドミントン部副顧問"],
+    ["先生", "3年2組の担任"],
+    ["先生", "2年学年主任　2年1組の担任　バドミントン部顧問"],
+    ["先生", ""],
+    ["先生", "国語の先生"],
+    ["先生", "美術の先生"],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""],
+    ["先生", ""]
 ];
 
 const gallery = document.getElementById("gallery");
@@ -24,22 +24,19 @@ const gallery = document.getElementById("gallery");
 teachers.forEach(teacher => {
 
     const name = teacher[0];
-    const image = teacher[1];
-    const detail = teacher[2];
+    const detail = teacher[1];
 
     gallery.innerHTML += `
-        <div class="card" onclick="showDetail('${name}', '${image}', '${detail}')">
-            <img src="${image}" alt="${name}">
+        <div class="card" onclick="showDetail('${name}', '${detail}')">
             <div class="name">${name}</div>
         </div>
     `;
 });
 
-function showDetail(name, image, detail){
+function showDetail(name, detail){
 
     document.getElementById("detail").innerHTML = `
         <h2>${name}</h2>
-        <img src="${image}" alt="${name}">
         <p>${detail}</p>
     `;
 }
